adb7af6457176e3a3ac85d5c8856689630cac4f5
Rami Alrwais <raalrwai@ucsc.edu>
2020-10-20 13:13:57 -0700

    Deploying Mern App to Heroku

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+import EditExercise from "./components/edit-exercise.component";
+import ExercisesList from "./components/exercises-list.component";
+import CreateDiet from "./components/create-diet.component";
+import Navbar from "./components/navbar.component";
+
+/* import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
+import { useAuth0 } from '@auth0/auth0-react'; */
+
+function App() {
+  return (
+    <>
+   <Router>
+      <div className = "container">
+   <Navbar />
+      <br/>
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
+        <Route path="/diet" component={CreateDiet} />
+      </div>
+    </Router>
+    
+    </>
+  );
+
+}
+export default App;
