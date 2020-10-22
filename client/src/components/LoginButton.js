076e68826deeee935f50fa06e443eaad30f8fd05
Rami Alrwais <raalrwai@ucsc.edu>
2020-10-22 14:20:21 -0700

    testing proxy working for dev and prod

--- client/src/components/LoginButton.js
+++ client/src/components/LoginButton.js
@@ -1,7 +1,7 @@
 import "./app.css";
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-
+//this is working for both dev and prod
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
